45dea942ce65908d630c23f3babc85e1318a0702
Piotrek <kurdapiotr99@icloud.com>
2022-11-17 16:03:51 +0100

    File upload

--- Client/file-upload/src/app/app.component.ts
+++ Client/file-upload/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { Subscription, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'file-upload';
+
+  @Input() requiredFileType:string = 'txt';
+
+    fileName = '';
+    uploadProgress:number = 0;
+    uploadSub: Subscription = new Subscription;
+
+    constructor(private http: HttpClient) {}
+
+    onFileSelected(event: any) {
+        const file:File = event.target.files[0];
+        console.log(file);
+        if (file) {
+            this.fileName = file.name;
+            const formData = new FormData();
+            formData.append("thumbnail", file);
+
+            const upload$ = this.http.post("/api/thumbnail-upload", formData, {
+                reportProgress: true,
+                observe: 'events'
+            })
+            .pipe(
+                finalize(() => this.reset())
+            );
+          
+            this.uploadSub = upload$.subscribe(event => {
+              if (event.type == HttpEventType.UploadProgress) {
+                if(event.total)
+                this.uploadProgress = Math.round(100 * (event.loaded / event.total));
+              }
+            })
+        }
+    }
+
+  cancelUpload() {
+    this.uploadSub.unsubscribe();
+    this.reset();
+  }
+
+  reset() {
+    this.uploadProgress = 0;
+    //this.uploadSub = null;
+  }
 }
--- Client/file-upload/src/app/app.module.ts
+++ Client/file-upload/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
