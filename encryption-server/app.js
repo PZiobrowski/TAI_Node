48bb13063652635a78d87e713dc795bb548b98ba
przestr <struminski.przemyslaw@gmail.com>
2022-12-18 14:01:16 +0100

    add first impl of encryption and decryption endpoints

--- encryption-server/app.js
+++ encryption-server/app.js
@@ -4,8 +4,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
+const crypto = require('crypto');
+const { fstat } = require('fs');
+const fs = require('fs');
 const app = express()
 const port = 3000
+
+const key = process.env.KEY
+key = crypto.createHash('sha256').update(String(key)).digest('base64').substr(0, 32);
+
+const salt = process.env.SALT
+salt = crypto.randomBytes(8).toString("hex")
 
 app.use(fileUpload({
   createParentPath: true
@@ -45,6 +54,74 @@ app.post('/upload/no-encryption', async (req, res) => {
   }
 });
 
+
+app.post('/upload/encryption', async (req, res) => {
+  try {
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'No file uploaded'
+          });
+      } else {
+          let file = req.files.file;
+
+          const encrypted = cryptFileWithSalt(file, false);
+
+          fs.createWriteStream('./uploads/' + file.name).write(encrypted)
+
+          //send response
+          res.send({
+              status: true,
+              message: 'File is uploaded',
+              data: {
+                  name: file.name,
+                  mimetype: file.mimetype,
+                  size: file.size
+              }
+          });
+      }
+  } catch (err) {
+      console.log(err)
+      res.status(500).send(err);
+  }
+});
+
+app.post('/upload/decryption', async (req, res) => {
+  try {
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'No file uploaded'
+          });
+      } else {
+          let file = req.files.file;
+
+          console.log("dupa1")
+
+          const decrypted = cryptFileWithSalt(file, true);
+
+          console.log("dupa2")   
+
+          //send response
+          if (file) {
+            res.writeHead(200, {
+                'Content-Type': file.mimetype,
+                'Content-disposition': 'attachment;filename=' + 'encrypted_' + file.name,
+                'Connection': 'close',
+            })
+        } else {
+            res.writeHead(200, {
+                'Connection': 'close'
+            })
+        }
+        res.end(decrypted);
+      }
+  } catch (err) {
+      console.log(err)
+      res.status(500).send(err);
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -52,3 +129,19 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+const cryptFileWithSalt = (
+  file,
+  decrypt = false
+) => {
+  if (!decrypt) {
+    const cipher = crypto.createCipheriv('aes-256-ctr', key, salt);
+    const crypted = Buffer.concat([cipher.update(file.data), cipher.final()]);
+    return crypted;
+  } else {
+    const cipher = crypto.createDecipheriv('aes-256-ctr', key, salt);
+    const decrypted = Buffer.concat([cipher.update(file.data), cipher.final()]);
+    return decrypted;
+  }
+};
