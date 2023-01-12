04e588bbb0ee5efb064a9635f6ee61654e11832e
Piotrek <kurdapiotr99@icloud.com>
2023-01-12 14:32:12 +0100

    Podpięcie endpointów z szyfrowaniem i deszyfrowaniem

--- Client/file-upload/src/app/service/file-upload.service.ts
+++ Client/file-upload/src/app/service/file-upload.service.ts
@@ -9,40 +9,74 @@ import { BehaviorSubject, finalize } from 'rxjs';
 export class FileUploadService {
 
   fileUploaded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  uploadedFileName$: BehaviorSubject<string> = new BehaviorSubject<string>("");
   loading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
 
   uploadFile(data: File | null) {
+    this.loading$.next(true);
     const file = (data == null) ? null : this._prepareData(data);
     this.http.post("http://localhost:3000/upload/no-encryption", file)
-    .pipe()
+    .pipe(finalize(() => this.loading$.next(false)))
     .subscribe({
-      next: ((response) => {this.openToast("Udało się!")}),
+      next: ((response: any) => {
+        this.openToast(`Udało się! Czas - ${response.data.time}ms`);
+        this.fileUploaded$.next(true);
+        this.uploadedFileName$.next(response.data.name);
+      }),
       error: ((error) => {this.openToast("Wystąpił błąd!")})
     });
   }
 
-  encryptFileRSA(data: File) {
-
+  downloadFile() {
+    const formData = new FormData();
+    formData.append("fileName", this.uploadedFileName$.getValue());
+    this.http.post("http://localhost:3000/download", formData)
+    .pipe(finalize(() => this.loading$.next(false)))
+    .subscribe({
+      next: ((response: any) => {
+        this.openToast('Udało się!');
+      }),
+      error: ((error) => {this.openToast("Wystąpił błąd!")})
+    });
   }
 
-  streamFileRSA(data: File) {
-
-  }
-
-
-  encryptFileAES(data: File | null) {
+  encryptFile(data: File | null, type: string) {
+    this.loading$.next(true);
     const file = (data == null) ? null : this._prepareData(data);
+    file?.append('algorithm', type);
     this.http.post("http://localhost:3000/upload/encryption", file)
-    .pipe()
+    .pipe(finalize(() => this.loading$.next(false)))
     .subscribe({
-      next: ((response) => {this.openToast("Udało się!")}),
+      next: ((response: any) => {
+        this.openToast(`Udało się! Czas - ${response.data.time}ms`);
+        this.fileUploaded$.next(true);
+        this.uploadedFileName$.next(response.data.name);
+      }),
       error: ((error) => {this.openToast("Wystąpił błąd!")})
     });
   }
 
-  streamFileAES(data: File) {
+  decryptFile(data: File | null, type: string) {
+    this.loading$.next(true);
+    const file = (data == null) ? null : this._prepareData(data);
+    file?.append('algorithm', type);
+    this.http.post("http://localhost:3000/upload/decryption", file)
+    .pipe(finalize(() => this.loading$.next(false)))
+    .subscribe({
+      next: ((response: any) => {
+        this.openToast(`Udało się! Czas - ${response.data.time}ms`);
+      }),
+      error: ((error) => {this.openToast("Wystąpił błąd!")})
+    });
+  }
+
+  encryptStream(data: File | null, type: string) {
+
+  }
+
+  decryptStream(data: File | null, type: string) {
 
   }
 
