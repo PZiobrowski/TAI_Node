3cd67d8f8f4107bb030aa7b81d8eb7c388c29570
przestr <struminski.przemyslaw@gmail.com>
2023-01-12 16:01:18 +0100

    move crypto related code to separate file

--- encryption-server/app.js
+++ encryption-server/app.js
@@ -1,30 +1,12 @@
+const cryptoUtils = require('./utils/cryptoUtils.js')
 const express = require('express')
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const crypto = require('crypto');
 const fs = require('fs');
 const app = express()
 const port = 3000
-
-const key = crypto.createHash('sha256').update(String(process.env.KEY)).digest('base64').slice(0, 32);
-const salt = process.env.SALT || crypto.randomBytes(8).toString("hex");
-
-const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-  // The standard secure default length for RSA keys is 2048 bits
-  modulusLength: 2048,
-});
-
-const exportedPublicKeyBuffer = publicKey.export({
-  type: "pkcs1",
-  format: "pem",
-});
-
-const exportedPrivateKeyBuffer = privateKey.export({
-  type: "pkcs1",
-  format: "pem",
-});
 
 app.use(fileUpload({
   createParentPath: true
@@ -83,11 +65,11 @@ app.post('/upload/encryption', async (req, res) => {
 
           if(req.body.algorithm == '0') {
             console.log("encryption with aes")
-            encrypted = cryptFileAESWithSalt(file, false);
+            encrypted = cryptoUtils.cryptFileAESWithSalt(file, false);
           } else 
           {
             console.log("encryption with rsa")
-            encrypted = cryptFileRSA(file, false);
+            encrypted = cryptoUtils.cryptFileRSA(file, false);
           }
 
           const duration = (performance.now() - start).toPrecision(2)
@@ -131,11 +113,11 @@ app.post('/upload/decryption', async (req, res) => {
 
           if(req.body.algorithm == '0') {
             console.log("decryption with aes")
-            decrypted = cryptFileAESWithSalt(file, true);
+            decrypted = cryptoUtils.cryptFileAESWithSalt(file, true);
           } else 
           {
             console.log("decryption with rsa")
-            decrypted = cryptFileRSA(file, true);
+            decrypted = cryptoUtils.cryptFileRSA(file, true);
           }
 
           const duration = (performance.now() - start).toPrecision(2)
@@ -187,62 +169,9 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  //fs.writeFileSync("public.pem", exportedPublicKeyBuffer, { encoding: "utf-8" });
-  // fs.writeFileSync("private.pem", exportedPrivateKeyBuffer, {
+  //fs.writeFileSync("public.pem", cryptoUtils.exportedPublicKeyBuffer, { encoding: "utf-8" });
+  // fs.writeFileSync("private.pem", cryptoUtils.exportedPrivateKeyBuffer, {
   //   encoding: "utf-8",
   // });
   console.log(`Example app listening on port ${port}`)
 })
-
-
-const cryptFileAESWithSalt = (
-  file,
-  decrypt = false
-) => {
-  if (!decrypt) {
-    const cipher = crypto.createCipheriv('aes-256-ctr', key, salt);
-    const crypted = Buffer.concat([cipher.update(file.data), cipher.final()]);
-    return crypted;
-  } else {
-    const cipher = crypto.createDecipheriv('aes-256-ctr', key, salt);
-    const decrypted = Buffer.concat([cipher.update(file.data), cipher.final()]);
-    return decrypted;
-  }
-};
-
-
-const cryptFileRSA = (
-  file,
-  decrypt = false
-) => {
-  if (!decrypt) {
-  
-    const publicKey = Buffer.from(
-      fs.readFileSync("public.pem", { encoding: "utf-8" })
-    ); 
-    const encryptedData = crypto.publicEncrypt(
-      {
-        key: publicKey,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
-      },
-      // We convert the data string to a buffer using `Buffer.from`
-      Buffer.from(file.data)
-    );
-    return encryptedData;
-  } else {
-    const privateKey = fs.readFileSync("private.pem", { encoding: "utf-8" });
-    const decryptedData = crypto.privateDecrypt(
-      {
-        key: privateKey,
-        // In order to decrypt the data, we need to specify the
-        // same hashing function and padding scheme that we used to
-        // encrypt the data in the previous step
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
-      },
-      Buffer.from(file.data)
-    );
-    return decryptedData;
-  }
-};
