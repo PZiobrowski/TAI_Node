3a6a9a8ac759ce4b75f1d002248df59d1bdbb446
kmolski <krzysztof.molski29@gmail.com>
2023-01-18 17:47:27 +0100

    Add streaming RSA encryption/decryption

--- encryption-server/utils/cryptoUtils.js
+++ encryption-server/utils/cryptoUtils.js
@@ -62,6 +62,46 @@ const cryptFileAESWithSalt = (
     }
   };
 
+  const streamCryptFileRSA = (
+    file,
+    outStream,
+    decrypt = false
+  ) => {
+    if (decrypt) {
+      const privateKey = Buffer.from(fs.readFileSync("private.pem", { encoding: "utf-8" }));
+
+      const encryptedBuffer = Buffer.from(file.data);
+      for (let i = 0; i < encryptedBuffer.length; i += 256) {
+        const encrypted = encryptedBuffer.subarray(i, i + 256);
+        const chunk = crypto.privateDecrypt(
+          {
+            key: privateKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: "sha256"
+          },
+          encrypted
+        );
+        outStream.write(chunk);
+      }
+    } else {
+      const publicKey = Buffer.from(fs.readFileSync("public.pem", { encoding: "utf-8" }));
+
+      const fileBuffer = Buffer.from(file.data);
+      for (let i = 0; i < fileBuffer.length; i += 128) {
+        const chunk = fileBuffer.subarray(i, Math.min(i + 128, fileBuffer.length));
+        const encrypted = crypto.publicEncrypt(
+          {
+            key: publicKey,
+            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+            oaepHash: "sha256"
+          },
+          chunk
+        );
+        outStream.write(encrypted);
+      }
+    }
+  };
+
   const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
     // The standard secure default length for RSA keys is 2048 bits
     modulusLength: 2048,
@@ -77,4 +117,4 @@ const cryptFileAESWithSalt = (
     format: "pem",
   });
 
-  module.exports = {cryptFileAESWithSalt, cryptFileRSA, exportedPublicKeyBuffer, exportedPrivateKeyBuffer}
+  module.exports = {cryptFileAESWithSalt, cryptFileRSA, exportedPublicKeyBuffer, exportedPrivateKeyBuffer, streamCryptFileRSA}
